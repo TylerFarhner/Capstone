3c4dab9b014bfaf3308e1e5b3cd2316cf4ef7af3
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-20 06:26:53 -0800

    finishes hamburger menu

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import SpotsListScreen from '../screens/SpotsListScreen'
 import SpotsDetailsScreen from '../screens/SpotsDetailsScreen'
 import FavoritesScreen from '../screens/FavoritesScreen'
+import AboutScreen from '../screens/AboutScreen'
+import ContactScreen from '../screens/ContactScreen'
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+const HeaderLeft = () => {
+    // now have access to all props from react navigation
+    const navigation = useNavigation();
+
+    return (
+        <MaterialIcons name="menu" size={24} onPress={() => { navigation.openDrawer() }} />
+    );
+}
 
 function HomeNavigator() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerLeft: () => <HeaderLeft />
+            }}
+        >
             <Stack.Screen 
                 name="SpotsList" 
                 component={ SpotsListScreen }
@@ -30,33 +47,72 @@ function HomeNavigator() {
 
 function FavoritesNavigator() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerLeft: () => <HeaderLeft />
+            }}
+        >
             <Stack.Screen name="Favorites" component={ FavoritesScreen } />
         </Stack.Navigator>
     )
 }
 
-function AppNavigator() {
+function AboutNavigator() {
     return (
-        <NavigationContainer>
-            <Tabs.Navigator
+        <Stack.Navigator
+            screenOptions={{
+                headerLeft: () => <HeaderLeft />
+            }}
+        >
+            <Stack.Screen name="About" component={ AboutScreen } />
+        </Stack.Navigator>
+    )
+}
+
+function ContactNavigator() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerLeft: () => <HeaderLeft />
+            }}
+        >
+            <Stack.Screen name="Contact" component={ ContactScreen } />
+        </Stack.Navigator>
+    )
+}
+
+function TabsNavigator() {
+    return (
+        <Tabs.Navigator
                 screenOptions={({ route }) => ({
                     tabBarIcon: () => {
                         let iconName;
                         if(route.name==="Home") {
                             iconName="home"
                         } else if (route.name==="Favorites") {
-                            iconName="favorite"
+                            iconName='favorite'
                         }
 
-                        return <MaterialIcons name={ iconName } size={ 24 } />
+                        return <MaterialIcons name={ iconName } size={24} />
 
                     }
                 })}
             >
+                {/* Passing in HomeNavigator (above) as a screen to this Home Tab (below) */}
                 <Tabs.Screen name="Home" component={ HomeNavigator } />
                 <Tabs.Screen name="Favorites" component={ FavoritesNavigator } />
             </Tabs.Navigator>
+    )
+}
+
+function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen name="Home" component={ TabsNavigator } />
+                <Drawer.Screen name="About" component={ AboutNavigator } />
+                <Drawer.Screen name="Contact" component={ ContactNavigator } />
+            </Drawer.Navigator>
         </NavigationContainer>
     )
 }
