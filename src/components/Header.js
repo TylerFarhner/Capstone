2572d1ccacc5f7d70636d9ea04bdc1ba8138d529
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-20 04:20:32 -0800

    adds header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Header() {
+    return (
+        <View style={ styles.header }>
+            <Text style={ styles.title }>Hangloose</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#486b00',
+        padding: 15,
+        borderBottomColor: '#2e4600',
+        borderBottomWidth: 1
+    },
+    title: {
+        marginTop: 40,
+        textAlign: 'center',
+        fontSize: 20,
+        color: '#ffffff' 
+    }
+})
