be1e3a864a66332053dc664a783bf291b210c38e
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-20 05:06:01 -0800

    styles card, adds icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Card() {
     return (
         <View style={ styles.card }>
             <View style={ styles.titleWrapper }>
-                <Text>title</Text>
+                <Text style={ styles.title }>Spot Name</Text>
+                <MaterialIcons 
+                    name="favorite-border" 
+                    color="#A2c523"
+                    size={24}
+                />
             </View>
             <View style={ styles.locationWrapper }>
-                <Text>location</Text>
+                <Text style={ styles.location }>location</Text>
             </View>
             <View style={ styles.descriptionWrapper }>
-                <Text>description</Text>
+                <Text style={ styles.description }>description</Text>
             </View>
         </View>
     )
@@ -51,6 +57,19 @@ const styles = StyleSheet.create({
 
     descriptionWrapper: {
         paddingHorizontal: 15
+    },
+
+    title: {
+        fontSize: 20
+    },
+
+    location: {
+        fontSize: 15,
+        marginTop: 10
+    },
+
+    description: {
+        marginTop: 10
     }
 
 })
